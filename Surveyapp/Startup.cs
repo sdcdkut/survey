8707c9604b5dc415e0347869af5d3f92bc51d1de
kanyijoshua <kanyiwest.jo@gmail.com>
2022-12-07 13:00:09 +0300

    hide error page in production

--- Surveyapp/Startup.cs
+++ Surveyapp/Startup.cs
@@ -330,7 +330,7 @@ namespace Surveyapp
             {
                 MinimumSameSitePolicy = SameSiteMode.Lax
             });*/
-            app.UseDeveloperExceptionPage();
+            //app.UseDeveloperExceptionPage();
             app.UseSession();
             if (env.IsDevelopment())
             {
@@ -342,7 +342,6 @@ namespace Surveyapp
             {
                 //app.UseDeveloperExceptionPage();
                 app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
-                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
